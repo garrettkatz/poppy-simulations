870ef85badcb10108baf75d79172420ac1780c5d
garrettkatz <garrett.katz@gmail.com>
2022-06-01 17:28:24 -0400

    fixed training bug

--- ergo/pybullet/tasks/walking/babble.py
+++ ergo/pybullet/tasks/walking/babble.py
@@ -13,73 +13,80 @@ from goals import sample_goal, goal_distance
 if __name__ == "__main__":
 
     show = False
+    dotrain = True
 
     traj_len = 2
 
-    num_episodes = 10
+    num_episodes = 1000
     num_steps = 1
 
     s_sigma = 0.01
     a_sigma = 0.01
+    learning_rate = 0.0005
 
-    # launches the simulator
-    env = PoppyErgoEnv(pb.POSITION_CONTROL, use_fixed_base=False, show=show)
-    env.set_base(orn = pb.getQuaternionFromEuler((0,0,np.pi)))
+    if dotrain:
 
-    num_inp = len(env.joint_index) + 26
-    num_hid = 64
-    num_out = traj_len*len(env.joint_index)
-
-    net = tr.nn.Sequential(
-        tr.nn.Linear(num_inp, num_hid),
-        tr.nn.LeakyReLU(),
-        tr.nn.Linear(num_hid, num_out),
-    )
-    opt = tr.optim.SGD(net.parameters(), lr=0.001)
-
-    rewards = np.empty((num_episodes, num_steps))
-    sl_losses = np.empty((num_episodes, num_steps))
-
-    for episode in range(num_episodes):
-
-        env.reset()
+        # launches the simulator
+        env = PoppyErgoEnv(pb.POSITION_CONTROL, use_fixed_base=False, show=show)
         env.set_base(orn = pb.getQuaternionFromEuler((0,0,np.pi)))
+    
+        num_inp = len(env.joint_index) + 26
+        num_hid = 64
+        num_out = traj_len*len(env.joint_index)
 
-        for step in range(num_steps):
+        net = tr.nn.Sequential(
+            tr.nn.Linear(num_inp, num_hid),
+            tr.nn.LeakyReLU(),
+            tr.nn.Linear(num_hid, num_out),
+        )
+        opt = tr.optim.SGD(net.parameters(), lr=learning_rate)
+    
+        rewards = np.empty((num_episodes, num_steps))
+        sl_losses = np.empty((num_episodes, num_steps))
+    
+        for episode in range(num_episodes):
+    
+            env.reset()
+            env.set_base(orn = pb.getQuaternionFromEuler((0,0,np.pi)))
+    
+            for step in range(num_steps):
+    
+                old_base = env.get_base()
+                g = sample_goal(env)
+                sg = np.concatenate((env.get_position(),) + old_base + g)
+                inp = sg + np.random.randn(len(sg)) * s_sigma
+    
+                out = net(tr.tensor(inp).float())
+                dst = tr.distributions.normal.Normal(out, a_sigma)
+                a = dst.sample()
+                log_prob = dst.log_prob(a).sum()
+    
+                traj = a.detach().numpy().reshape((traj_len, -1))
+                for target in traj: env.goto_position(target)
+                new_base = env.get_base()
+    
+                reward = np.exp(-goal_distance(new_base, g))
+                rl_loss = -reward * log_prob
+                rl_loss.backward()
+    
+                sn = np.concatenate((env.get_position(),) + old_base + new_base)
+                sl_loss = tr.sum((net(tr.tensor(sn).float()) - a.detach())**2)
+                sl_loss.backward()
+    
+                opt.step()
+                opt.zero_grad()
+    
+                rewards[episode, step] = reward
+                sl_losses[episode, step] = sl_loss.item()
+    
+            print(f"ep {episode}: rl {rewards[episode].mean():e}, sl {sl_losses[episode].mean():e}")
+    
+        env.close()
+    
+        np.savez("babble.npz", rewards=rewards, sl_losses=sl_losses)
 
-            old_base = env.get_base()
-            g = sample_goal(env)
-            sg = np.concatenate((env.get_position(),) + old_base + g)
-            inp = sg + np.random.randn(len(sg)) * s_sigma
-
-            out = net(tr.tensor(inp).float())
-            dst = tr.distributions.normal.Normal(out, a_sigma)
-            a = dst.sample()
-            log_prob = dst.log_prob(a).sum()
-
-            traj = a.detach().numpy().reshape((traj_len, -1))
-            for target in traj: env.goto_position(target)
-            new_base = env.get_base()
-
-            reward = np.exp(-goal_distance(new_base, g))
-            rl_loss = -reward * log_prob
-            rl_loss.backward()
-
-            sn = np.concatenate((env.get_position(),) + old_base + new_base)
-            sl_loss = tr.sum((net(tr.tensor(sn).float()) - a.detach())**2)
-            sl_loss.backward()
-
-            opt.zero_grad()
-            opt.step()
-
-            rewards[episode, step] = reward
-            sl_losses[episode, step] = sl_loss.item()
-
-        print(f"ep {episode}: rl {rewards[episode].mean()}, sl {sl_losses[episode].mean()}")
-
-    env.close()
-
-    np.savez("babble.npz", rewards=rewards, sl_losses=sl_losses)
+    npz = np.load("babble.npz")
+    rewards, sl_losses = npz["rewards"], npz["sl_losses"]
 
     pt.subplot(1,2,1)
     pt.plot(rewards)
