445c9faeb975a1b46b292fd6a971831cbb1079c9
garrettkatz <garrett.katz@gmail.com>
2022-10-20 11:53:06 -0400

    first draft base trajectory generator

---
+++ ergo/pybullet/tasks/walking/base_trajectory.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as pt
+
+def get_traj(y0, y1, dy0, dy1, T):
+
+    D = y0
+    C = dy0
+    A, B = np.linalg.solve(
+        np.array([[1,1],[3,2]]),
+        np.stack((y1 - y0 - dy0, dy1 - dy0)))
+
+    s = np.linspace(0, 1, T)[:,np.newaxis]
+    t = (1 - np.cos(np.pi * s)) / 2
+    Y = A*t**3 + B*t**2 + C*t + D
+
+    # if y0.ndim == 1: y0 = y0[:,np.newaxis]
+    # if y1.ndim == 1: y1 = y1[:,np.newaxis]
+
+    # t = np.linspace(0, 1, T)
+    # Y = -2*(y1 - y0)*t**3 + 3*(y1 - y0)*t**2 + y0
+
+    return Y.T, s.flatten()
+
+if __name__ == "__main__":
+
+    y0 = np.array([0, 0])
+    y1 = np.array([-4, 1])
+    dy0 = np.array([0, 1])
+    dy1 = np.array([1, 0])
+    Y, t = get_traj(y0, y1, dy0, dy1, 100)
+
+    pt.subplot(2,1,1)
+    pt.plot(Y[0], Y[1], '.-')
+
+    pt.subplot(2,1,2)
+    dY = np.linalg.norm((Y[:,1:] - Y[:,:-1]) / (t[1:] - t[:-1]), axis=0)
+    pt.plot(t[:-1], dY, '.-')
+
+    pt.show()
+
+
