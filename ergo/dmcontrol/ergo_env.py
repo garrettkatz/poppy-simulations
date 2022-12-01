3c9c6ac478943eed6bd1d61ab64b6a2aa6be8610
garrettkatz <garrett.katz@gmail.com>
2022-12-01 11:04:30 -0500

    initial dmcontrol env can stand a while with untuned kp etc

--- ergo/dmcontrol/ergo_env.py
+++ ergo/dmcontrol/ergo_env.py
@@ -1,14 +1,15 @@
 import os
 from dm_control import mjcf
+from dm_env.specs import BoundedArray
+from dm_env import Environment, StepType, TimeStep
 
-class ErgoEnv:
+class ErgoDomain:
     def __init__(self, control_period):
         self.control_period = control_period # number of physics steps per action
         self.physics = None
         # physics.model.nq
 
-    def get_observation(self):
-        return self.physics.data.qpos.copy()
+        # self._action_spec = BoundedArray(shape=(31,), dtype=float)
 
     def reset(self):
 
@@ -16,46 +17,85 @@ class ErgoEnv:
         # first do `mujoco/bin/path/compile poppy_ergo.dmcontrol.urdf poppy_ergo.dmcontrol.xml`
         # mjcf_path = os.path.join("..","urdfs","ergo","poppy_ergo.dmcontrol.xml")
         mjcf_path = os.path.join("..","urdfs","ergo","poppy_ergo.dmcontrol.mod.xml")
-        arena = mjcf.from_path(mjcf_path)
+        self.arena = mjcf.from_path(mjcf_path)
 
         # add floor from https://arxiv.org/pdf/2006.12983.pdf
-        checker = arena.asset.add(
+        checker = self.arena.asset.add(
             'texture', type='2d', builtin='checker', width=300, height=300, rgb1=[.2, .3, .4], rgb2=[.3, .4, .5])
-        grid = arena.asset.add('material', name='grid', texture=checker, texrepeat=[5,5], reflectance=.2)
-        arena.worldbody.add('geom', type='plane', size=[2, 2, .1], material=grid)
+        grid = self.arena.asset.add('material', name='grid', texture=checker, texrepeat=[5,5], reflectance=.2)
+        self.arena.worldbody.add('geom', type='plane', size=[2, 2, .1], material=grid)
+
+        # extract joint information for action spec
+        actuators = self.arena.find_all("actuator")
+        joints = [a.joint for a in actuators]
+        ranges = [j.range for j in joints]
+        self._action_spec = BoundedArray(
+            shape=(len(joints),),
+            dtype=float,
+            minimum=[r[0] for r in ranges],
+            maximum=[r[1] for r in ranges],
+        )
 
         # set up physics and return initial observation
-        self.physics = mjcf.Physics.from_mjcf_model(arena)
-        return self.get_observation()
+        self.physics = mjcf.Physics.from_mjcf_model(self.arena)
+
+    def action_spec(self):
+        return self._action_spec
 
     def step(self, action=None):
         for _ in range(self.control_period):
             self.physics.step()
-        return self.get_observation()
+
+class ErgoEnv(Environment):
+    def __init__(self, control_period):
+        self.domain = ErgoDomain(control_period)
+
+    def action_spec(self):
+        return self.domain.action_spec()
+
+    def observation_spec(self):
+        # same as action spec for position joint control
+        return self.domain.action_spec()
+
+    def get_observation(self):
+        return self.domain.physics.data.qpos.copy()
+
+    def reset(self):
+        self.domain.reset()
+        self.physics = self.domain.physics
+        reward, discount = 0, None
+        return TimeStep(StepType.FIRST, reward, discount, self.get_observation())
+
+    def step(self, action=None):
+        self.domain.step(action)
+        reward, discount = 0, 1
+        return TimeStep(StepType.MID, reward, discount, self.get_observation())
 
 
 if __name__ == "__main__":
 
-    import matplotlib.pyplot as pt
     
     env = ErgoEnv(control_period = 10)
-    obs = env.reset()
+    ts = env.reset()
 
-    # maybe through pymjcf, and/or with urdf extensions (also for meshdir):
-    # https://mujoco.readthedocs.io/en/latest/modeling.html?highlight=urdf#urdf-extensions
+    import numpy as np
+    def zero_policy(ts):
+        # print(ts.observation)
+        np.zeros(env.action_spec().shape)
 
-    pt.ion()
-    pt.show()
-    for t in range(50):
-        obs = env.step()
-        pixels = env.physics.render()
+    from dm_control import viewer
+    viewer.launch(env, zero_policy)
 
-        pt.cla()
-        pt.imshow(pixels)
-        pt.pause(.01)
-
-        print(t)
-        # input(t)
-
-    pt.close()
+    # import matplotlib.pyplot as pt
+    # pt.ion()
+    # pt.show()
+    # for t in range(50):
+    #     obs = env.step()
+    #     pixels = env.physics.render()
+    #     pt.cla()
+    #     pt.imshow(pixels)
+    #     pt.pause(.01)
+    #     print(t)
+    #     # input(t)
+    # pt.close()
 
