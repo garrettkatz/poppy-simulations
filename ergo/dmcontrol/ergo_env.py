9227e1be8dd2f1407bfeda9db39fedef2323acf8
garrettkatz <garrett.katz@gmail.com>
2022-12-01 12:21:46 -0500

    looking into domain randomization and kp tuning

--- ergo/dmcontrol/ergo_env.py
+++ ergo/dmcontrol/ergo_env.py
@@ -9,15 +9,18 @@ class ErgoDomain:
         self.physics = None
         # physics.model.nq
 
-        # self._action_spec = BoundedArray(shape=(31,), dtype=float)
-
-    def reset(self):
+    def reset(self, kp = None):
 
         # load ergo model
         # first do `mujoco/bin/path/compile poppy_ergo.dmcontrol.urdf poppy_ergo.dmcontrol.xml`
         # mjcf_path = os.path.join("..","urdfs","ergo","poppy_ergo.dmcontrol.xml")
         mjcf_path = os.path.join("..","urdfs","ergo","poppy_ergo.dmcontrol.mod.xml")
         self.arena = mjcf.from_path(mjcf_path)
+
+        # Domain tweak
+        if kp is not None:
+            for p in range(len(self.arena.actuator.position)):
+                self.arena.actuator.position[p].kp = kp[p]
 
         # add floor from https://arxiv.org/pdf/2006.12983.pdf
         checker = self.arena.asset.add(
@@ -60,8 +63,8 @@ class ErgoEnv(Environment):
     def get_observation(self):
         return self.domain.physics.data.qpos.copy()
 
-    def reset(self):
-        self.domain.reset()
+    def reset(self, kp = None):
+        self.domain.reset(kp)
         self.physics = self.domain.physics
         reward, discount = 0, None
         return TimeStep(StepType.FIRST, reward, discount, self.get_observation())
@@ -74,18 +77,31 @@ class ErgoEnv(Environment):
 
 if __name__ == "__main__":
 
-    
-    env = ErgoEnv(control_period = 10)
-    ts = env.reset()
-
     import numpy as np
+
+    env = ErgoEnv(control_period = 10)
+
+    kp = 30*np.ones(31)
+    # kp = 20 + 30000*np.ones(31)
+    ts = env.reset(kp)
+
+    # model names, but changes don't seem to affect phys
+    # env.physics.named.model.dof_armature
+    # env.physics.named.model.dof_damping
+    # env.physics.named.model.actuator_gainprm
+    # env.physics.named.model.actuator_biasprm
+    # env.physics.named.model.body_inertia
+    # print(env.physics.named.model.actuator_gainprm)
+
     def zero_policy(ts):
         # print(ts.observation)
         np.zeros(env.action_spec().shape)
 
+    # using viewer with conformant env
     from dm_control import viewer
     viewer.launch(env, zero_policy)
 
+    # # raw matplotlib viz without conformant env
     # import matplotlib.pyplot as pt
     # pt.ion()
     # pt.show()
