48c4401c05c5ef83b049b63bb8693f79b8345a73
garrettkatz <garrett.katz@gmail.com>
2023-06-28 02:39:47 -0400

    progress on leg model

--- ergo/pybullet/tasks/walking/physics_tests.py
+++ ergo/pybullet/tasks/walking/physics_tests.py
@@ -1,3 +1,5 @@
+import numpy as np
+import time
 import pybullet as pb
 
 import pybullet_data
@@ -5,34 +7,43 @@ import pybullet_data
 client = pb.connect(pb.GUI)
 pb.setAdditionalSearchPath(pybullet_data.getDataPath())
 pb.setGravity(0,0,-10)
-planeId = pb.loadURDF("plane.urdf")
+plane_id = pb.loadURDF("plane.urdf")
+robot_id = pb.loadURDF("test.urdf")#, basePosition=(0,0,2))
 
-cid = pb.createCollisionShape(pb.GEOM_SPHERE, radius=.25)
-vid = pb.createVisualShape(pb.GEOM_SPHERE, radius=.25)
+joint_idx = {}
+for i in range(pb.getNumJoints(robot_id)):
+    name = pb.getJointInfo(robot_id, i)[1].decode('utf-8')
+    joint_idx[name] = i
+print(joint_idx)
 
-mid = pb.createMultiBody(
-    baseMass=0,
-    baseCollisionShapeIndex=cid,
-    baseVisualShapeIndex=vid,
-    baseOrientation=(0,0,1,0),
-    linkMasses = [1],
-    linkCollisionShapeIndices=[cid],
-    linkVisualShapeIndices=[vid],
-    linkPositions=[(0,0,1)],
-    linkOrientations=[(.7071,0,.7071,0)],
-    linkInertialFramePositions=[(0,0,1)],
-    linkInertialFrameOrientations=[(0,0,0,1)],
-    linkParentIndices=[0],
-    linkJointTypes=[pb.JOINT_REVOLUTE],
-    linkJointAxis=[(1,0,0,0)],
-)
+pb.resetBasePositionAndOrientation(robot_id, (0,0,2.5), (0,0,0,1))
+pb.resetJointState(robot_id, joint_idx["left_hip"], -np.pi/6)
+pb.resetJointState(robot_id, joint_idx["right_hip"], -np.pi/6)
+pb.resetJointState(robot_id, joint_idx["left_ankle"], np.pi/6)
+pb.resetJointState(robot_id, joint_idx["right_ankle"], np.pi/6)
 
-pb.resetJointState(mid, 0, 1.)
+for i in range(len(joint_idx)):
+    pb.setJointMotorControl2(robot_id, i, pb.TORQUE_CONTROL, force=0.)
 
-# for _ in range(100):
-#     input('.')
-while True:
+# input('.')
+for _ in range(100):
     pb.stepSimulation()
+    time.sleep(0.01)
+
+input('.')
+
+pb.enableJointForceTorqueSensor(robot_id, joint_idx["right_ankle"])
+
+# pb.setJointMotorControl2(robot_id, joint_idx["left_ankle"], pb.POSITION_CONTROL, targetPosition=np.pi/8)
+# while True:
+for _ in range(100):
+    pb.setJointMotorControl2(robot_id, joint_idx["right_ankle"], pb.TORQUE_CONTROL, force=0.)
+    for i in range(len(joint_idx)):
+        pb.setJointMotorControl2(robot_id, i, pb.TORQUE_CONTROL, force=0.)
+    pb.stepSimulation()
+    time.sleep(0.01)
+    print(pb.getJointState(robot_id, joint_idx["right_ankle"]))
+    # input('.')
 
 pb.disconnect()
 
