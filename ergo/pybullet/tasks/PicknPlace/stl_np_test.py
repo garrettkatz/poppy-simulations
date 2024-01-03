a2311ae182ffb2b9ee782e5a58abed153a003642
Ringostarr <akshay@fspl.in>
2024-01-03 10:39:37 -0500

    Updated stl Test
    With new urdf and diff resolutions.

--- ergo/pybullet/tasks/PicknPlace/stl_np_test.py
+++ ergo/pybullet/tasks/PicknPlace/stl_np_test.py
@@ -1,7 +1,7 @@
 # Before running this script, need to run: pip install --user stl-to-voxel
 
 # stl-to-voxel parameters, higher resolution is slower, especially visualization
-resolution = 20
+resolution = 5
 voxel_size = None
 parallel = False
 
@@ -9,10 +9,23 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 from stl import mesh
+
 from stltovoxel.convert import convert_mesh
 
+def export_npy_mod(voxels, scale, shift):
+    voxels = voxels.astype(bool)
+    out = []
+    for z in range(voxels.shape[0]):
+        for y in range(voxels.shape[1]):
+            for x in range(voxels.shape[2]):
+                if voxels[z][y][x]:
+                    point = (np.array([x, y, z]) / scale) + shift
+                    out.append(point)
+    return out
 # download Rubber_Duck.stl from : https://www.thingiverse.com/download:2336973
-input_file_path = os.path.join(os.environ["HOME"], "Downloads", "Rubber_Duck.stl")
+#input_file_path = os.path.join(os.environ["HOME"], "Downloads", "Rubber_Duck.stl")
+
+input_file_path = "C:\\Users\\Ringo\\Documents\\GitHub\\poppy-simulations\\Manipulation\\meshes\\duck2.stl"
 
 # based on https://github.com/cpederkoff/stl-to-voxel/blob/master/stltovoxel/convert.py
 print("converting...")
@@ -22,11 +35,12 @@ vol, scale, shift = convert_mesh(org_mesh, resolution, voxel_size, parallel)
 
 # duck happens to be oriented on its side, rotate upright
 vol = np.rot90(vol, axes=(0,2))
-
+voxels = export_npy_mod(vol,scale,shift)
 # visualize, based on https://matplotlib.org/stable/gallery/mplot3d/voxels.html
 print("\nvisualizing...")
 ax = plt.figure().add_subplot(projection='3d')
 ax.voxels(vol, facecolors='y', edgecolor='k')
-plt.axis("equal")
+plt.axis("auto")
 plt.show()
 
+print()
